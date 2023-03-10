1d4b026843265e872e2cc2625f22574ee2d4e7c6
Ryan Migaud <rmigaud@asu.edu>
2023-03-10 14:08:54 -0800

    half.c implementation

--- c/half/half.c
+++ c/half/half.c
@@ -1,22 +1,40 @@
 // Calculate your half of a restaurant bill
 // Data types, operations, type casting, return value
 
-#include <cs50.h>
+// #include <cs50.h>
 #include <stdio.h>
 
 float half(float bill, float tax, int tip);
 
 int main(void)
 {
-    float bill_amount = get_float("Bill before tax and tip: ");
-    float tax_percent = get_float("Sale Tax Percent: ");
-    int tip_percent = get_int("Tip percent: ");
+    // float bill_amount = get_float("Bill before tax and tip: ");
+    float bill_amount;
+    printf("Bill before tax and tip: ");
+    scanf("%f", &bill_amount);
+    // printf("Bill: %.2f.", bill_amount);
 
+    // float tax_percent = get_float("Sale Tax Percent: ");
+    float tax_percent;
+    printf("Sale Tax Percent: ");
+    scanf("%f", &tax_percent);
+    // printf("tax_percent: %.2f.", tax_percent);
+
+
+    // int tip_percent = get_int("Tip percent: ");
+    int tip_percent;
+    printf("Tip percent: ");
+    scanf("%i", &tip_percent);
+    // printf("tip_percent: %.2f", tip_percent);
+    
     printf("You will owe $%.2f each!\n", half(bill_amount, tax_percent, tip_percent));
 }
 
 // TODO: Complete the function
 float half(float bill, float tax, int tip)
 {
-    return 0.0;
+    float p_tax = (tax / 100.0) +1.0;
+    float p_tip =( tip / 100.0) +1.0;
+
+    return( ((bill * p_tax) * p_tip ) /2.0);
 }
---
+++ c/half/half.md
@@ -0,0 +1,32 @@
+[cs50: half.c](https://cs50.harvard.edu/x/2023/problems/1/half/)
+
+## How to run
+
+To run on windows with a gcc compiler and make installed, open a powershell and navigate to half.c and type the following command:
+
+```
+make half CC=gcc ; ./half
+```
+
+## Example output
+
+```
+Bill before tax and tip: 12.50
+Sale Tax Percent: 8.875
+Tip percent: 20
+You will owe $8.17 each!
+```
+
+```
+Bill before tax and tip: 23.50
+Sale Tax Percent: 7
+Tip percent: 15
+You will owe $14.46 each!
+```
+
+```
+Bill before tax and tip: 100
+Sale Tax Percent: 6.25
+Tip percent: 18
+You will owe $62.69 each!
+```
